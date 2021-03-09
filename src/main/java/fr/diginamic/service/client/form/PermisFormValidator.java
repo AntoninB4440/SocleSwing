7ae8b8a1fb72ef38aab8e8f1d0db6d13d6305ba0
AntoninB4440 <barceloantonin@gmail.com>
2021-03-09 10:18:55 +0100

    PermisFormValidator okai

--- src/main/java/fr/diginamic/service/client/form/PermisFormValidator.java
+++ src/main/java/fr/diginamic/service/client/form/PermisFormValidator.java
@@ -1,13 +1,29 @@
 package fr.diginamic.service.client.form;
 
+import java.time.LocalDate;
+
 import fr.diginamic.composants.ui.Form;
 import fr.diginamic.composants.validator.FormValidator;
+import fr.diginamic.service.utils.DateUtils;
 
 public class PermisFormValidator extends FormValidator {
 
 	@Override
 	public boolean validate(Form form) {
 		// TODO Auto-generated method stub
+
+		String numeroPermis = form.getValue("champNumeroPermis");
+		LocalDate dateObtention = DateUtils.parseLocal(form.getValue("champDateObtention"));
+
+		if (numeroPermis.isEmpty()) {
+			console.alert("Le numéro du permis est obligatoire");
+			return false;
+		}
+
+		if (dateObtention.isAfter(LocalDate.now())) {
+			console.alert("La date d'obtention ne peut pas être supérieur à la date d'aujourd'hui");
+			return false;
+		}
 		return true;
 	}
 
